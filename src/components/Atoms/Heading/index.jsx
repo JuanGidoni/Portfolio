f18d684ca453e766522fa0fe1914c2fe03e8263e
juangidoni <juan.gidoni@gmail.com>
2021-08-29 20:09:09 -0300

    #1: Atomic design first steps

---
+++ src/components/Atoms/Heading/index.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+const Heading = () => {
+ return (
+  <>
+
+  </>
+ )
+}
+
+export default Heading
---
+++ src/components/Atoms/Link/index.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+const Link = () => {
+ return (
+  <>
+
+  </>
+ )
+}
+
+export default Link
